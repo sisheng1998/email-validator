2402a6cbce1964a6ffb055149a22cf0cf7a29fbb
Sheng <sisheng1998@gmail.com>
2024-05-11 15:17:46 +0800

    fix vercel deployment issue

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { handle } from '@hono/node-server/vercel'
 
-const app = new Hono().basePath('/api')
+const app = new Hono()
 
 app.get('/', (c) => c.json({ message: 'Hello Hono!' }))
 
